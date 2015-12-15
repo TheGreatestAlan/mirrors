4171a851157452106fcad791daa34825c4db2254
TheGreatestAlan <alan.hoang.nguyen@gmail.com>
2015-12-14 22:41:22 -0700

    built data structure for products and variants

--- test.py
+++ test.py
@@ -1,11 +1,30 @@
 import csv
+from collections import defaultdict
 #with open('C:\Users\Gorgak\Downloads\CSV\test', 'rb') as csvfile:
 #    spamreader = csv.reader(csvfile, delimiter=',', quotechar="\"")
 #    for row in spamreader:
 #        print ', '.join(row)
 
+csv_path = __file__ + "\\..\\resource\\ShopifyWithVariants.csv"
+image_path = __file__ + "\\..\\resource\\images\\"
+handles = defaultdict(list)
 
-file = open(__file__+"\\..\\resource\\keys.properties",'r')
-
-print file.readline()
-print file.readline()
+print csv_path
+with open(csv_path,'rb') as f:	
+	rownum = 0
+	reader = csv.reader(f)
+	for row in reader:
+#		if rownum == 1:
+		if rownum >= 1:
+			#print row[0]
+			if row[0] not in handles:
+				print "ADDING " + row[0] + " TO HANDLES"
+				handles[row[0]] = [[]]
+				for column in row:
+					handles[row[0]][0].append(column)
+				handles[row[0]].append(row)
+#			if not row[1]
+		rownum += 1
+	for key in handles:
+		print handles[key][0][3]
+		break
