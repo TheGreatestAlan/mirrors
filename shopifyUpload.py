e7c3965628b2f64cb9251af73433141fd9723fd8
TheGreatestAlan <alan.hoang.nguyen@gmail.com>
2015-12-19 19:51:03 -0700

    God. Damnit. Well I've got some of the variant stuff worked out,\n and the data structure is sussed out now

--- shopifyUpload.py
+++ shopifyUpload.py
@@ -18,53 +18,62 @@ csv_path = __file__ + "\\..\\resource\\ShopifyWithVariants.csv"
 image_path = __file__ + "\\..\\resource\\images\\"
 
 handles = defaultdict(list)
-#looping through csv
+handle_ids = dict()
+
+ #looping through csv
 with open(csv_path,'rb') as f:	
-	rownum = 0
-	reader = csv.reader(f)
-	for row in reader:
-#		if rownum == 1:
-		if rownum >= 1:
-			row_list = []
-			for column in row:
-				row_list.append(column)
-			if row[0] not in handles:
-				handles[row[0]] = []
-				handles[row[0]].append(row_list)
-			else:
-				handles[row[0]].append(row_list)
-		rownum += 1
+ 	rownum = 0
+ 	reader = csv.reader(f)
+ 	for row in reader:
+ #		if rownum == 1:
+ 		if rownum >= 1:
+ 			row_list = []
+ 			for column in row:
+ 				row_list.append(column)
+ 			if row[0] not in handles:
+ 				handles[row[0]] = []
+ 				handles[row[0]].append(row_list)
+ 			else:
+ 				handles[row[0]].append(row_list)
+ 		rownum += 1
 
+#Inserting products
 for product in handles:
-	#if the title is not null or empty (i.e. I'm taking this to be the base product)
-	print product
-	for variant in handles[product]:
-		print variant[8]
-#	if not handles[product][0][1]:
-#		print handles[product][0][8]
-
-'''
-	new_product = shopify.Product()
-	#add title, etc.
-	new_product.handle = row[0]
-	new_product.title = row[1]
-	new_product.save()
-	print(new_product.id)
-	new_product.body_html = row[2]
-	new_product.vendor = row[3]
-	new_product.type = row[4]
-	new_product.tags = row[5]
-#			new_product.description = row[28]
-	v = shopify.Variant(dict(price=row[19], option1="First", product_id=new_product.id))
-	new_product.variants = [v]
-	filename = image_path + row[24]
-	print filename
-#			    filename = path.split("\\")[-1:][0]
-	image = shopify.Image()
-	with open(filename, "rb") as f:
-	    image.attach_image(f.read(), filename=filename)
-	new_product.images = [image]
-	success = new_product.save()
-	print success
-
-'''
+ 	for variant in handles[product]:
+ 		if variant[1]:
+ 			print variant[42]
+ 		 	print variant[0]
+ 			new_product = shopify.Product()
+ 			new_product.handle = variant[0]
+ 			new_product.title = variant[1]
+ 			new_product.body_html = variant[2]
+ 			new_product.vendor = variant[3]
+ 			new_product.product_type = variant[4]
+ 			new_product.tags = variant[5]
+ 			filename = __file__ + "\\..\\resource\\images\\" + variant[42]
+ 			print filename
+ 			image = shopify.Image()
+ 			with open(filename, "rb") as f:
+ 			    image.attach_image(f.read(), filename=filename)
+ 			new_product.images = [image]
+ 			v = shopify.Variant(dict(price=variant[19], option1=variant[8], product_id=new_product.id))
+ 			new_product.variants = [v]
+ 			success = new_product.save()
+ 			handle_ids[variant[0]] = new_product.id		
+ 			print success
+ 	break
+#import variants
+for product in handles:
+ 	for variant in handles[product]:
+ 		if not variant[1]:
+ 			product_variant = shopify.Product.find(handle_ids[product])
+ 			v = shopify.Variant(dict(price=variant[19], option1=variant[8], product_id=product_variant.id))
+ 			v1 = shopify.Variant(dict(price=variant[19], option1="TEST", product_id=product_variant.id))
+ 			# vars = product_variant.variants
+ 			# if not vars:
+ 			# 	vars = v
+ 			# print len(vars)
+ 			product_variant.variants = [v, v1]
+ 			success = product_variant.save()
+ 			print success
+ 	break
